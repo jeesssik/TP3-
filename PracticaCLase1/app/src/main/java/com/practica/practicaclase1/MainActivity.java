2be28f06132cd019924c1a36cb9804ccd4dac73b
jeesssik <jeesssik@gmail.com>
2020-08-07 16:53:09 -0300

    boton que muestra mensaje@

---
+++ PracticaCLase1/app/src/main/java/com/practica/practicaclase1/MainActivity.java
@@ -0,0 +1,28 @@
+ package com.practica.practicaclase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnBoton1= findViewById(R.id.button);
+
+        btnBoton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //que hago cuando aprieto en boton
+                Toast.makeText(getApplicationContext(),"Test de App",Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
